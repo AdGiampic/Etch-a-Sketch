34d8d53dd86b2b6270c67b56ddaf8b97377a1ef8
Unknown-Drifter <adgiampic@gmail.com>
2023-01-21 16:13:01 +0100

    Modified style.css and added color-grid event in script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function createGrid16 () {
     const divGrid = document.createElement('div'); // create square
     divGrid.setAttribute('style', 'border: 1px solid gray; width:40px; height: 40px'); // square has style
+    divGrid.setAttribute('class', 'paint')
     for (let i = 1;i<=256; i++) { //test create square 5 times
         document.getElementById('board').appendChild(divGrid.cloneNode(true));
     }
 }
 
 createGrid16();
+
+const color = document.querySelectorAll('.paint') // select every single item
+color.forEach ((item) => { // cycle through the items
+    item.addEventListener('mouseover' ,() => { // for each item I add a click event
+        item.style.background = "black"
+    })
+})
+
