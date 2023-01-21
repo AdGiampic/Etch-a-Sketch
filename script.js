c301fc85ba9ecf125f22e9977916538bee7c3de2
Unknown-Drifter <adgiampic@gmail.com>
2023-01-21 14:38:09 +0100

    Managed to create grid based container and modified the layout of the board

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function createGrid16 () {
     const divGrid = document.createElement('div'); // create square
     divGrid.setAttribute('style', 'border: 1px solid gray; width:40px; height: 40px'); // square has style
-    for (let i = 1;i<=16; i++) { //test create square 5 times
+    for (let i = 1;i<=256; i++) { //test create square 5 times
         document.getElementById('board').appendChild(divGrid.cloneNode(true));
     }
 }
